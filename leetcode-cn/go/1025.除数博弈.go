782ff082495ecf58ec892ce69c07e095c9577c8d
aak1247 <aak1247@126.com>
2020-07-24 01:42:11 +0800

    1025 dp

--- leetcode-cn/go/1025.除数博弈.go
+++ leetcode-cn/go/1025.除数博弈.go
@@ -13,3 +13,24 @@ func divisorGame(N int) bool {
     }
     return divisorGame(N-2)
 }
+
+// n2 dp 小常数
+func divisorGame(N int) bool {
+	if N == 1 {
+			return false
+	}
+	if N == 2 {
+			return true
+	}
+	var dp = make([]bool, N+1)
+	dp[1] = false
+	dp[2] = true
+	for i := 3 ; i <= N ; i++ {
+		dp[i] = dp[i-2]||dp[i]
+		t := N / i
+		for j := 2 ; j <= t ; j++ {
+			dp[j*i]=dp[(j-1)*i]||dp[i]
+		}
+	}
+	return dp[N]
+}
